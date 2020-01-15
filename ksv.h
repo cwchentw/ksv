e70b3756b9b5161c4bb9896e7ff9fcf9e448520e
Michael Chen <cwchen123@gmail.com>
2020-01-15 16:43:16 +0800

    Update ksv.h

--- ksv.h
+++ ksv.h
@@ -24,8 +24,8 @@ extern "C" {
 
 ksv_t * ksv_new_default(void);
 ksv_t * ksv_new(char *delimeter, char *end_of_line, char quote);
-BOOL ksv_load_stream_with_header_strictly(ksv_t *self, FILE *stream);
 void ksv_delete(void *self);
+BOOL ksv_load_stream_with_header_strictly(ksv_t *self, FILE *stream);
 
 #ifdef __cplusplus
 }
