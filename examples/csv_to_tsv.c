d1b9cde1f90d6009239aa967fc0f69b03ae1e400
Michelle Chen <cwchen.tw@outlook.com>
2022-01-10 15:49:59 +0800

    Comment some examples

--- examples/csv_to_tsv.c
+++ examples/csv_to_tsv.c
@@ -15,24 +15,27 @@ int main(int argc, char *argv[])
 
     char *path = argv[1];
 
+    /* Open a CSV sheet. */
     fp = fopen(path, "r");
     if (!fp) {
         PUTERR("Unable to read file");
         return 1;
     }
 
+    /* Create a ksv object. */
     ksv = ksv_new_default();
     if (!ksv) {
         PUTERR("Failed to create ksv object");
         goto ERROR_MAIN;
     }
 
+    /* Load the header row of the sheet. */
     if (KSV_SUCCESS != ksv_load_header(ksv, fp)) {
         PUTERR("Failed to load sheet header");
         goto ERROR_MAIN;
     }
 
-    /* Load header and convert it. */
+    /* Iterate over the header(s) of the header row. */
     char *field = ksv_next_header(ksv);
     while (field) {
         PRINT("%s\t", field);
@@ -41,14 +44,18 @@ int main(int argc, char *argv[])
     }
     PUTS("\b");
 
+    /* Iterate over the sheet. */
     while (!feof(fp)) {
+        /* Load one row from the sheet. */
         if (KSV_SUCCESS != ksv_load_record(ksv, fp)) {
             PUTERR("Failed to load a sheet record");
             goto ERROR_MAIN;
         }
 
+        /* Re-init the state of the ksv object. */
         ksv_restart(ksv);
 
+        /* Iterate over the cell(s) of the row. */
         field = ksv_next_data_by_row(ksv);
         while (field) {
             PRINT("%s\t", field);
@@ -58,6 +65,7 @@ int main(int argc, char *argv[])
         PUTS("\b");
     }
 
+    /* Release system resources. */
     ksv_delete(ksv);
     fclose(fp);
 
--- examples/csv_to_tsv_cpp.cpp
+++ examples/csv_to_tsv_cpp.cpp
@@ -15,12 +15,14 @@ int main(int argc, char *argv[])
 
     char *path = argv[1];
 
+    /* Open a CSV sheet. */
     fp = fopen(path, "r");
     if (!fp) {
         std::cerr << "Unable to read file" << std::endl;
         return 1;
     }
 
+    /* Create a ksv object. */
     try {
         ksv = new KSV();
     }
@@ -28,11 +30,13 @@ int main(int argc, char *argv[])
         goto ERROR_MAIN;
     }
 
+    /* Load the header row of the sheet. */
     if (!ksv->load_header(fp)) {
         std::cerr << "Failed to load sheet header" << std::endl;
         goto ERROR_MAIN;
     }
 
+    /* Iterate over the header(s) of the header row. */
     field = ksv->next_header();
     while (!field.empty()) {
         std::cout << field << "\t";
@@ -41,14 +45,18 @@ int main(int argc, char *argv[])
     }
     std::cout << "\b" << std::endl;
 
+    /* Iterate over the sheet. */
     while (!feof(fp)) {
+        /* Load one row from the sheet. */
         if (!ksv->load_record(fp)) {
             std::cerr << "Failed to load a sheet record" << std::endl;
             goto ERROR_MAIN;
         }
 
+        /* Re-init the state of the ksv object. */
         ksv->restart();
 
+        /* Iterate over the cell(s) of the row. */
         field = ksv->next_data_by_row();
         while (!field.empty()) {
             std::cout << field << "\t";
@@ -58,6 +66,7 @@ int main(int argc, char *argv[])
         std::cout << "\b" << std::endl;
     }
 
+    /* Release system resources. */
     delete ksv;
     fclose(fp);
 
